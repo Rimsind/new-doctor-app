7e91a09a1f8f5d4398d4fbd573a6df3382468c07
Rims <83367739+Rimsind@users.noreply.github.com>
2022-01-11 06:07:30 -0800

    hghcsdgfvcfb

--- pages/home.js
+++ pages/home.js
@@ -7,6 +7,57 @@ const Home = () => {
   return (
     <>
       <h5 className="mb-0">Dashboard</h5>
+      <div className="row">
+        <div className="col-xl-3 col-lg-6 mt-4">
+          <div
+            className="card shadow border-0 p-3"
+            style={{ backgroundColor: "#a2f5ce" }}
+          >
+            <div className="d-flex justify-content-between ">
+              <h6 className="align-items-center mb-0">Total Patients</h6>
+              <h5>100</h5>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-xl-3 col-lg-6 mt-4">
+          <div
+            className="card shadow border-0 p-3"
+            style={{ backgroundColor: "#bfdbfe" }}
+          >
+            <div className="d-flex justify-content-between ">
+              <h6 className="align-items-center mb-0">
+                Patients under treatement
+              </h6>
+              <h5>100</h5>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-xl-3 col-lg-6 mt-4">
+          <div
+            className="card shadow border-0 p-3"
+            style={{ backgroundColor: "#fde68a" }}
+          >
+            <div className="d-flex justify-content-between ">
+              <h6 className="align-items-center mb-0">Urgent</h6>
+              <h5>100</h5>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-xl-3 col-lg-6 mt-4">
+          <div
+            className="card shadow border-0 p-3"
+            style={{ backgroundColor: "#ffdfdf" }}
+          >
+            <div className="d-flex justify-content-between ">
+              <h6 className="align-items-center mb-0">Canceled</h6>
+              <h5>100</h5>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
